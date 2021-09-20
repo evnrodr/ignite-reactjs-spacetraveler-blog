e6bf77e4617fcaecdcf9255bc6055d0982c5e311
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-09-19 23:06:13 -0300

    fix to prismic service

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,11 +1,10 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
-  if (process.env.PRISMIC_API_ENDPOINT) {
-    const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-      req,
-    });
+  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT ?? "", {
+    req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
 
-    return prismic;
-  }
+  return prismic;
 }
